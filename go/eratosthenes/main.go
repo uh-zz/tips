036c382e47d05c2c172bf82ee21d351afbbdf2ae
uh-zz <uhzz.contact@gmail.com>
2021-02-02 20:06:41 +0900

    add eratosthenes algorithm

---
+++ go/eratosthenes/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+// generateSeq シーケンシャルなスライスを生成
+func generateSeq(min, max int) ([]int, error) {
+	if max <= min {
+		fmt.Printf("input max: %d, min: %d\n", max, min)
+		return nil, errors.New("input error")
+	}
+
+	seq := make([]int, max-min+1)
+	for index := range seq {
+		seq[index] = min + index
+	}
+	return seq, nil
+}
+
+// searchEratosthenes エラトステネスのふるいで素数探索
+func searchEratosthenes(seq []int) []int {
+	var (
+		shift []int
+		prime int
+	)
+
+	for {
+
+		seq = append(seq[:0], seq[1:]...)
+
+		// step1
+		prime = seq[0]
+		shift = append(shift, prime)
+
+		// step2
+		search := make([]int, 0)
+		for _, val := range seq {
+			if val == 1 {
+				continue
+			}
+
+			if val%prime != 0 {
+				search = append(search, val)
+			}
+		}
+		seq = search
+
+		// step3
+		if float64(seq[0]) >= math.Sqrt(float64(seq[len(seq)-1])) {
+			// step4
+			shift = append(shift, seq...)
+			break
+		}
+	}
+	return shift
+}
+func main() {
+
+	seq, err := generateSeq(1, 120)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	prime := searchEratosthenes(seq)
+	fmt.Println(prime)
+}
