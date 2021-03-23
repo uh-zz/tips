1ba45c54599c6dbc07aca366489f6812a0301bad
NakZMichael <nakazato.muka@gmail.com>
2021-03-23 09:20:01 +0900

    add how to use SPA in correct way

--- react/todo_app/go.mod
+++ react/todo_app/go.mod
@@ -6,6 +6,7 @@ replace todo_app => ./
 go 1.15
 
 require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.1 // indirect
--- react/todo_app/pkg/todo/control/exec.go
+++ react/todo_app/pkg/todo/control/exec.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -48,7 +49,7 @@ func Exec() error {
 	// routerの設定
 	r := gin.Default()
 	// 静的ファイルのServe
-	r.Static("/web", "../web-ui/build")
+	r.Use(static.Serve("/", static.LocalFile("../web-ui/build", true)))
 
 	r.GET("/todo/all", func(c *gin.Context) {
 		todos := &models.Todos{}
@@ -105,7 +106,7 @@ func Exec() error {
 
 	})
 
-	err = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err = r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		return err
 	}
