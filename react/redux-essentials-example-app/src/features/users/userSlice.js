ae7bae2cded1a6cf04a2b24e489df780ca09b875
NakZMichael <nakazato.muka@gmail.com>
2021-04-16 04:13:13 +0900

    add eslint file

--- react/redux-essentials-example-app/src/features/users/userSlice.js
+++ react/redux-essentials-example-app/src/features/users/userSlice.js
@@ -1,29 +1,33 @@
-import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit"
-import { client } from "../../api/client"
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from '@reduxjs/toolkit';
+import {client} from '../../api/client';
 
-const usersAdapter = createEntityAdapter()
+const usersAdapter = createEntityAdapter();
 
-const initialState = usersAdapter.getInitialState()
+const initialState = usersAdapter.getInitialState();
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async()=>{
-    const response = await client.get('/fakeApi/users')
-    return response.users
-})
-
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async ()=>{
+  const response = await client.get('/fakeApi/users');
+  return response.users;
+});
 
 
 const userSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers:{},
-    extraReducers:{
-        [fetchUsers.fulfilled]: usersAdapter.setAll
-    }
-})
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchUsers.fulfilled]: usersAdapter.setAll,
+  },
+});
 
-export default userSlice.reducer
+
+export default userSlice.reducer;
 
 export const {
-    selectAll: selectAllUsers,
-    selectById: selectUserById,
-} = usersAdapter.getSelectors(state => state.users)
+  selectAll: selectAllUsers,
+  selectById: selectUserById,
+} = usersAdapter.getSelectors((state) => state.users);
