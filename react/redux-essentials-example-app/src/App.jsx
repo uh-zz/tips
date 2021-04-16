b427c944dd6ab26c3bf6909e0f62836b37163a45
NakZMichael <nakazato.muka@gmail.com>
2021-04-16 15:03:33 +0900

    fix a dependency bug of eslint

--- react/redux-essentials-example-app/src/App.jsx
+++ react/redux-essentials-example-app/src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-import { Navbar } from './app/Navbar'
-import { NotificationsList } from './features/notifications/NotificationsList'
-import { AddPostForm } from './features/posts/AddPostForm'
-import { EditPostForm } from './features/posts/EditPostForm'
-import { PostsList } from './features/posts/PostsLists'
-import { SinglePostPage } from './features/posts/SinglePostPage'
-import { UserList } from './features/users/UserList'
-import { UserPage } from './features/users/UserPage'
+import {Navbar} from './app/Navbar';
+import {NotificationsList} from './features/notifications/NotificationsList';
+import {AddPostForm} from './features/posts/AddPostForm';
+import {EditPostForm} from './features/posts/EditPostForm';
+import {PostsList} from './features/posts/PostsLists';
+import {SinglePostPage} from './features/posts/SinglePostPage';
+import {UserList} from './features/users/UserList';
+import {UserPage} from './features/users/UserPage';
 
 function App() {
+  const [isRed, setIsRed] = useState([false]);
+
   return (
     <Router>
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: isRed[0]?'red':'black',
+        }}
+        onClick={()=>{
+          setIsRed([!isRed[0]]);
+        }}
+      />
       <Navbar />
       <div className="App">
         <Switch>
@@ -40,7 +52,7 @@ function App() {
         </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
