36ca9bf5d62eda85c5c66b0c4c737f72356047cc
NakZMichael <nakazato.muka@gmail.com>
2021-04-21 04:43:16 +0900

    add next.js tutorial

--- react/hello-world/src/components/PostH1.tsx
+++
@@ -1,8 +0,0 @@
-import { Container, Typography } from '@material-ui/core';
-import React from 'react';
-
-export const H1 = ({children}:any)=>{
-  return (
-    <Typography variant="h2" style={{fontWeight:"bold"}}>{children}</Typography>
-  );
-};
