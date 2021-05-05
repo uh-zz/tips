c10001dd75233e6f283466fd80b51e944cd52eff
NakZMichael <nakazato.muka@gmail.com>
2021-05-05 18:00:27 +0900

    add timer view

--- react/music-kitchen/App.js
+++ react/music-kitchen/App.js
@@ -1,21 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { interval } from 'rxjs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+export default class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      isStarted:false,
+      timeSeconds:10,
+    }
+    this.intervalSubscriber = null
+  }
+
+
+  onPress = () => {
+    if(this.state.isStarted){
+      this.intervalSubscriber.unsubscribe()
+    }else{
+      this.intervalSubscriber = interval(1000).subscribe(
+        x=>{
+          if(this.state.timeSeconds > 0){
+            this.setState({timeSeconds:this.state.timeSeconds -1})
+          }else{
+            alert("Alert!")
+            this.intervalSubscriber.unsubscribe()
+            this.setState({isStarted:!this.state.isStarted})
+          }
+        }
+      )
+    }
+    this.setState({isStarted:!this.state.isStarted})
+  }
+
+  render = ()=> {
+    return(
+      <View style={this.styles.container}>
+        <Text>Set Time!</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => this.setState({timeSeconds:Number(text)})}
+          value={this.state.timeSeconds.toString()}
+        />
+        <Text>{this.state.timeSeconds}</Text>
+        <Button title={this.state.isStarted?'Stop':'Start'} onPress={ev => this.onPress() } />
+
+        <StatusBar style="auto" />
     </View>
-  );
+    )
+
+  }
+
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
