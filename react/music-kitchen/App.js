6c2a24c3a87e457b968eac8c6c2e771625600df9
NakZMichael <nakazato.muka@gmail.com>
2021-05-06 02:29:06 +0900

    reduce class component to react hooks

--- react/music-kitchen/App.js
+++ react/music-kitchen/App.js
@@ -1,65 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Button, StyleSheet, Text, View　} from 'react-native';
 import { interval } from 'rxjs';
+import { Item, Input, Label } from 'native-base'
 
 
-export default class App extends React.Component{
 
-  constructor(){
-    super()
-    this.state = {
-      isStarted:false,
-      timeSeconds:10,
-    }
-    this.intervalSubscriber = null
+
+export default App = () =>{
+  const [leftTimeSeconds,setLeftTimeSeconds] = useState(0)
+  const [isStarted,setIsStarted] = useState(false)
+
+  
+  const onPress = () =>{
+    setIsStarted(!isStarted)
   }
-
-
-  onPress = () => {
-    if(this.state.isStarted){
-      this.intervalSubscriber.unsubscribe()
-    }else{
-      this.intervalSubscriber = interval(1000).subscribe(
-        x=>{
-          if(this.state.timeSeconds > 0){
-            this.setState({timeSeconds:this.state.timeSeconds -1})
-          }else{
-            alert("Alert!")
-            this.intervalSubscriber.unsubscribe()
-            this.setState({isStarted:!this.state.isStarted})
-          }
+  
+  const intervalSubscriber = useRef()
+  useEffect(() => {
+    // useEffectはisStartedの値が変更された時しか実行されないので
+    // 代わりにintervalの中で参照する値valueを定義する必要がある。
+    let value = leftTimeSeconds
+    if(isStarted){
+      intervalSubscriber.current = interval(1000).subscribe(x=>{
+        if(value === 0){
+          alert("Time up!")
+          intervalSubscriber.current.unsubscribe()
+          return
         }
-      )
+        value--
+        setLeftTimeSeconds(value)
+      })
+      return () => {
+        intervalSubscriber.current.unsubscribe()
+      };
+    }else{
+      return ()=>{
+      }
     }
-    this.setState({isStarted:!this.state.isStarted})
-  }
+  },[isStarted]);
 
-  render = ()=> {
-    return(
-      <View style={this.styles.container}>
-        <Text>Set Time!</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => this.setState({timeSeconds:Number(text)})}
-          value={this.state.timeSeconds.toString()}
-        />
-        <Text>{this.state.timeSeconds}</Text>
-        <Button title={this.state.isStarted?'Stop':'Start'} onPress={ev => this.onPress() } />
 
-        <StatusBar style="auto" />
-    </View>
+
+  
+  const leftTimeDisplay = isStarted?<Text>{leftTimeSeconds}</Text>:(
+    <Item floatingLabel style={styles.input}>
+            <Label>Set Seconds</Label>
+            <Input 
+              value={leftTimeSeconds.toString()}
+              keyboardType="numbers-and-punctuation" 
+              onChangeText={
+                (ev)=>{
+                  let value = Number(ev)
+                  if(isNaN(value) || value < 0 ){
+                    value = 0 
+                  }
+                  setLeftTimeSeconds(value)
+                }
+              }
+            />
+      </Item>
+    
     )
-
-  }
-
-  styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }
-  })
+    
+    return(
+      <View style={styles.container}>
+        {leftTimeDisplay}
+        <Button title={isStarted?'Stop':'Start'} onPress={ev => onPress() } />
+        <StatusBar style="auto" />
+      </View>
+  )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width:200,
+    height:40,
+    margin:50,
+  },
+})
